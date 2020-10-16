a9cfe227ff91790e6f6f46c65af41007da342708
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2020-10-16 17:46:57 -0300

    New go run

--- 4_Desafio_de_CI/src/soma_test.go
+++ 4_Desafio_de_CI/src/soma_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	result := soma(5, 4)
+	result := soma(5, 5)
 
 	if result != 10 {
 		t.Error("Soma falhou! O resultado esperado é 10")
